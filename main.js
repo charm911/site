b7eca85b4764cd3f949d1c378f55ff8be158b0dc
Nouriel Arazi <narazi@gmail.com>
2023-10-23 22:57:20 -0400

    add language toggle

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 function increment() {
   const divElement = document.getElementById("clicks");
-
   var clicks = parseInt(divElement.innerHTML);
-
   console.log("nbClicks = ", clicks);
-
   divElement.innerHTML = ++clicks;
+}
 
+let languageContent = {
+  "en": {
+    greeting: "Welcome to the site stored in the not FRC repo!",
+    clickme: "Click me!"
+  },
+  "fr": {
+    greeting: "Bienvenue sur le site depuis le repo not FRC site!",
+    clickme: "Clickez moi!"
+  }
+}
+
+
+function switchLang(lang) {
+  for (let key in languageContent[lang]) {
+    document.getElementById(key).innerHTML = languageContent[lang][key];
+  }
 }
